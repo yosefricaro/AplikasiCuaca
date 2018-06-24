af6f0e6314ff15acc6f5febc5f4a157eb1f28812
yosefricaro <yosefricarolatif@yahoo.com>
2018-06-24 12:08:45 +0700

    Initial commit

---
+++ app/src/main/java/com/yosef/aplikasicuaca/ForecastFragment.java
@@ -0,0 +1,25 @@
+package com.yosef.aplikasicuaca;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+public class ForecastFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_forecast, container, false);
+        ButterKnife.bind(this, rootView);
+
+
+
+        return rootView;
+    }
+
+}
---
+++ app/src/main/java/com/yosef/aplikasicuaca/MainActivity.java
@@ -0,0 +1,39 @@
+package com.yosef.aplikasicuaca;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, new Fragment())
+                .commit();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            getSupportFragmentManager().popBackStack();
+        } else if (!doubleBackToExitPressedOnce) {
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this,"Silahkan tekan kembali untuk keluar", Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(() -> doubleBackToExitPressedOnce = false, 2000);
+        } else {
+            super.onBackPressed();
+            return;
+        }
+    }
+}
